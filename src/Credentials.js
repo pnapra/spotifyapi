747e18e8e50a4559fec9ed40eae56906cdb8b814
pnapra <arpanalwayswins@gmail.com>
2021-11-29 11:16:20 -0500

    Committing changes

---
+++ src/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+  return {
+    ClientId: "30346d23fda448158964f570f5c1b612",
+    ClientSecret: "92afb89e49ab4e2588f80df0a9e31a44",
+  };
+};
+
+export { Credentials };
