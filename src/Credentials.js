abfd09d96d312c998de629f5d6beb00648969fcc
pnapra <arpanalwayswins@gmail.com>
2021-11-29 11:30:38 -0500

    Update Credentials.js

--- src/Credentials.js
+++ src/Credentials.js
@@ -1,7 +1,7 @@
 const Credentials = () => {
   return {
-    ClientId: "30346d23fda448158964f570f5c1b612",
-    ClientSecret: "92afb89e49ab4e2588f80df0a9e31a44",
+    ClientId: "",
+    ClientSecret: "",
   };
 };
 
